50bed9ab79505286150510529171e96fb09708b1
zeshuaro <joshuaystang@gmail.com>
2019-10-27 00:11:43 +1100

    Add to draw and send charts of stats (#27)

--- group_defender/stats.py
+++ group_defender/stats.py
@@ -1,3 +1,10 @@
+import matplotlib
+
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import tempfile
+
 from collections import defaultdict
 from google.cloud import datastore
 
@@ -24,6 +31,7 @@ def update_stats(chat_id, counts):
 def get_stats(update, _):
     query = client.query(kind=CHAT)
     counts = defaultdict(int)
+    total = 0
 
     for chat in query.fetch():
         if chat.key.id > 0:
@@ -34,16 +42,29 @@ def get_stats(update, _):
         for file_type in FILE_TYPES:
             if file_type in chat:
                 counts[file_type] += chat[file_type]
+                total += chat[file_type]
 
-    text = f'Number of users: {counts["num_users"]}\nNumber of groups: {counts["num_groups"]}\n\n'
-    total = 0
+    update.effective_message.reply_text(
+        f'Number of users: {counts["num_users"]}\nNumber of groups: {counts["num_groups"]}\n'
+        f"Total processed: {total}"
+    )
+    send_plot(update, counts)
 
-    for file_type in FILE_TYPES:
-        if file_type in counts:
-            text += f"Processed {file_type}: {counts[file_type]}\n"
-            total += counts[file_type]
-        else:
-            text += f"Processed {file_type}: 0\n"
 
-    text += f"\nTotal processed: {total}"
-    update.effective_message.reply_text(text)
+def send_plot(update, counts):
+    nums = [counts[x] for x in FILE_TYPES]
+    x_pos = list(range(len(FILE_TYPES)))
+
+    plt.rcdefaults()
+    _, ax = plt.subplots()
+
+    ax.bar(x_pos, nums, align="center")
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(FILE_TYPES, rotation=45)
+    ax.set_xlabel("File Types")
+    ax.set_ylabel("Counts")
+    plt.tight_layout()
+
+    with tempfile.NamedTemporaryFile(suffix=".png") as tf:
+        plt.savefig(tf.name)
+        update.effective_message.reply_photo(open(tf.name, "rb"))
